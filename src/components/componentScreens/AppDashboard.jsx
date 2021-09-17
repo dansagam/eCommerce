e2368d944cef9bb2e0e53d23ff1f069088ad7bef
dansagam <olukayodeogunnowo@yahoo.com>
2021-09-17 21:50:02 +0100

    adding the paginate fromt eh code

--- src/components/componentScreens/AppDashboard.jsx
+++ src/components/componentScreens/AppDashboard.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import AppCarousels from '../componentParts/AppCarousels'
+import AppLoader from '../componentParts/AppLoader'
+import Message from '../componentParts/Message'
+import MetaData from '../componentParts/MetaData'
+import PaginationContainer from '../componentParts/PaginationContainer'
+import Product from '../componentParts/Product'
 
-const AppDashboard = () => {
+const AppDashboard = ({match}) => {
+   const keyword = match.params.keyword
+   // const pageNumber = match.params.pageNumber
    return (
-      <div>
-         <h2>testing microphone</h2>
-         
-      </div>
+      <>
+         <MetaData />
+         <AppCarousels />
+         <h1>Latest Products</h1>
+         <AppLoader />
+         <Message variant='danger'>ferjskfj</Message>
+         <>
+            <Row>
+               <Col sm={12} md={6} lg={4} xl={3}>
+                  <Product product='product' />
+               </Col>
+            </Row>
+            <PaginationContainer 
+               pages={'ewee'}
+               page= {'plojj'}
+               keyword={keyword ? keyword : ''}
+            />
+         </>
+      </>
    )
 }
 
