910dc5c339fb5c7976b94422cce88d444966de15
dansagam <olukayodeogunnowo@yahoo.com>
2021-09-18 15:29:09 +0100

    create the redux store for the applicaition

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+export default configureStore({
+   reducer: {
+      Cart: 'dsdskd',
+      User: 'alds',
+      Order: 'dalsjd',
+      Product: 'flsajdals'
+   }
+})
