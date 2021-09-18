7ff5bc5d2cc64bb068815f58ec1937e87ab4f317
dansagam <olukayodeogunnowo@yahoo.com>
2021-09-18 14:58:15 +0100

    rating component crated

--- src/components/componentParts/Ratings.jsx
+++ src/components/componentParts/Ratings.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faStar, faStarHalfAlt} from '@fortawesome/free-solid-svg-icons'
 
-const Ratings = () => {
+const Ratings = ({value, text, color}) => {
+   
+   
    return (
-      <div>
-         <p>field testing</p>
+      <div className='rating'>
+         <span>
+            <FontAwesomeIcon icon={ 
+               value >= 1 ? faStar 
+               : value >= 0.5 ? faStarHalfAlt 
+               : faStar } style={{color}} 
+            />
+         </span>
+         <span>
+            <FontAwesomeIcon icon={ 
+               value >= 2 ? faStar 
+               : value >= 1.5 ? faStarHalfAlt 
+               : faStar } style={{color}} 
+            />
+         </span>
+         <span>
+            <FontAwesomeIcon icon={ 
+               value >= 3 ? faStar 
+               : value >= 2.5 ? faStarHalfAlt 
+               : faStar } style={{color}} 
+            />
+            
+         </span>
+         <span>
+            <FontAwesomeIcon icon={ 
+               value >= 4 ? faStar 
+               : value >= 3.5 ? faStarHalfAlt 
+               : faStar } style={{color}} 
+            />
+            
+         </span>
+         <span>
+            <FontAwesomeIcon icon={ 
+               value >= 5 ? faStar 
+               : value >= 4.5 ? faStarHalfAlt 
+               : faStar } style={{color}} 
+            />
+         </span>
+         <span>{text && text}</span>
       </div>
    )
+}
+
+Ratings.defaultProps ={
+   color: '#f8e825',
 }
 
 export default Ratings
