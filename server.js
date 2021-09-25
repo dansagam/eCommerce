55e53beb7d2cc763ff7824dc16bb01d54779b6a2
dansagam <olukayodeogunnowo@yahoo.com>
2021-09-25 23:33:54 +0100

    declaration of db and adding new dependencies

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
-import express from "express"
+import express from 'express'
 import path from 'path'
 import { config } from 'dotenv'
-import morgan from 'morgan'
-const app = express()
+import logger from 'morgan'
+import connectDB from './config/dB.js'
+
 
 config({path: './config/config.env'})
+
+connectDB()
+
+const app = express()
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'))
+}
+
 app.use(express.json())
-const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    )
+}else {
+    app.get('/', (req, res) =>{
+        res.send('API is running')
+    })
+}
+
+
+const PORT = process.env.PORT || 5000
 app.listen(
     PORT, () => 
-    console.log(`Budget income app  ${process.env.NODE_ENV} listening on port ${PORT} 🔥 !` )
+    console.log(`server running in  ${process.env.NODE_ENV} mode on port ${PORT} 🔥 !` )
 )
 
