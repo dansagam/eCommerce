8158c587f2537f453703470ceda0060fedb3e528
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-16 10:41:35 +0100

    modified the image upload model of the productcontrollers

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ import orderRoutes from './routes/orderRoutes.js'
 import storeRoutes from './routes/storeRoutes.js'
 import reviewRoutes from './routes/reviewRoutes.js'
 import cartRoutes from './routes/cartRoutes.js'
+import imageRoutes from './routes/uploadRoutes.js'
 import { errorHandler, notFound } from './middlewares/errorMiddlewares.js'
 
 
-config({path: './config/config.env'})
+config({ path: './config/config.env' })
 
 connectDB()
 
@@ -30,19 +31,20 @@ app.use('/api/users', userRoutes)
 app.use('/api/carts', cartRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/stores', storeRoutes)
+app.use('/api/productimageupload', imageRoutes)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
 
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     )
-}else {
-    app.get('/', (req, res) =>{
+} else {
+    app.get('/', (req, res) => {
         res.send('API is running')
     })
 }
@@ -53,7 +55,7 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
-    PORT, () => 
-    console.log(`server running in  ${process.env.NODE_ENV} mode on port ${PORT} 🔥 !` )
+    PORT, () =>
+    console.log(`server running in  ${process.env.NODE_ENV} mode on port ${PORT} 🔥 !`)
 )
 
