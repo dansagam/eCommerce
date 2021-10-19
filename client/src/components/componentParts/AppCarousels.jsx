6e4db7aa9ea4f3377f2f06c9e63fa0f39378db8a
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-19 21:51:11 +0100

    import the top product rating to carousel and create the logic for the header

--- client/src/components/componentParts/AppCarousels.jsx
+++ client/src/components/componentParts/AppCarousels.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Image, Carousel } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { getTopRatedProduct } from '../../reduxReducers/asyncReducers/productAsyncReducers'
 import AppLoader from './AppLoader'
 import Message from './Message'
 
 const AppCarousels = () => {
+   const dispatch = useDispatch()
    const productList = useSelector(state => state.Product)
-   const {loading, error, products} = productList
+   const { loading, error, topProducts } = productList
 
-   
-   return loading  ? (
+   useEffect(() => {
+      dispatch(getTopRatedProduct())
+   }, [dispatch])
+   return loading ? (
       <AppLoader />
    ) : error ? (
       <Message variant='danger' >{error}</Message>
    ) : (
       <>
          <Carousel pause='hover' className='bg-dark carousel' fade>
-            {products.map((product) => (
-            <Carousel.Item key={product._id} className='carousel-item'>
-               <Link to={`/product/${product._id}`}>
-                  <Image
-                     className="d-block"
-                     src={`${product.image}`}
-                     alt="First slide"
-                     fluid
-                  />
-                  <Carousel.Caption className='carousel-caption'>
-                     <h3>{product.name} {product.price}</h3>
-                  </Carousel.Caption>
-               </Link>
-            </Carousel.Item>
+            {topProducts.map((product) => (
+               <Carousel.Item key={product._id} className='carousel-item'>
+                  <Link to={`/product/${product._id}`}>
+                     <Image
+                        className="d-block"
+                        src={`${product.image}`}
+                        alt="First slide"
+                        fluid
+                     />
+                     <Carousel.Caption className='carousel-caption'>
+                        <h3>{product.name} {product.price}</h3>
+                     </Carousel.Caption>
+                  </Link>
+               </Carousel.Item>
 
             ))}
          </Carousel>
-         
+
       </>
    )
 }
--- client/src/components/componentParts/AppHeader.jsx
+++ client/src/components/componentParts/AppHeader.jsx
@@ -1,13 +1,52 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Nav, NavDropdown, Navbar, Container, Image } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../../images/eShoping.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faUser, faCartPlus} from '@fortawesome/free-solid-svg-icons'
+import { faUser, faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import { Route } from 'react-router'
 import SearchContainer from './SearchContainer'
+import { logoutSuccess } from '../../reduxReducers/userReducers'
 
 const AppHeader = () => {
+   const dispatch = useDispatch()
+   const { isAuthenticated } = useSelector(state => state.User)
+   const { userInfo } = useSelector(state => state.User.userLogin)
+
+   const logoutHandler = () => {
+      dispatch(logoutSuccess())
+   }
+
+   const profileLink = (
+      <NavDropdown title={userInfo ? userInfo.name : ''} id="navbarScrollingDropdown">
+         <LinkContainer to='/profile'>
+            <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
+         </LinkContainer>
+         <NavDropdown.Item href="#action4" onClick={logoutHandler}>Logout</NavDropdown.Item>
+         <NavDropdown.Divider />
+         {/* <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item> */}
+      </NavDropdown>
+   )
+   const loginLink = (
+      <LinkContainer to='/login' >
+         <Nav.Link ><FontAwesomeIcon icon={faUser} /> Sign in</Nav.Link>
+      </LinkContainer>
+   )
+   const adminLink = (
+      <NavDropdown title='Admin' id='adminmenu'>
+         <LinkContainer to='/admin/userlist'>
+            <NavDropdown.Item>Users</NavDropdown.Item>
+         </LinkContainer>
+         <LinkContainer to='/admin/productlist'>
+            <NavDropdown.Item>Product</NavDropdown.Item>
+         </LinkContainer>
+         <LinkContainer to='/admin/orderlist'>
+            <NavDropdown.Item>Order</NavDropdown.Item>
+         </LinkContainer>
+      </NavDropdown>
+   )
+
    return (
       <header>
          <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
@@ -17,7 +56,7 @@ const AppHeader = () => {
                </Navbar.Brand>
                <Navbar.Toggle aria-controls="navbarScroll" />
                <Navbar.Collapse className="justify-content-end" id="navbarScroll">
-                  <Route render={({history}) => <SearchContainer history={history} />} />
+                  <Route render={({ history }) => <SearchContainer history={history} />} />
                   <Nav
                      className="ml-auto my-2 my-lg-0"
                      style={{ maxHeight: '100px' }}
@@ -26,28 +65,23 @@ const AppHeader = () => {
                      <LinkContainer to='/cart'>
                         <Nav.Link><FontAwesomeIcon icon={faCartPlus} />Cart</Nav.Link>
                      </LinkContainer>
-                     <LinkContainer to='/login' >
-                        <Nav.Link ><FontAwesomeIcon icon={faUser} /> Sign in</Nav.Link>
-                     </LinkContainer>
-                     <LinkContainer to='/register' >
+                     {isAuthenticated ? profileLink
+                        : userInfo ? profileLink
+                           : loginLink
+                     }
+                     {userInfo && userInfo.isAdmin && adminLink}
+
+                     {/* <LinkContainer to='/register' >
                         <Nav.Link>Register</Nav.Link>
                      </LinkContainer>
-                     <NavDropdown title="Username  " id="navbarScrollingDropdown">
-                        <LinkContainer to='/profile'>
-                           <NavDropdown.Item href="#action3">Profile</NavDropdown.Item>
-                        </LinkContainer>
-                        <NavDropdown.Item href="#action4">Logout</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                     </NavDropdown>
                      <Nav.Link href="#" disabled>
                         Link
-                     </Nav.Link>
+                     </Nav.Link> */}
                   </Nav>
                </Navbar.Collapse>
             </Container>
          </Navbar>
-         
+
       </header>
    )
 }
