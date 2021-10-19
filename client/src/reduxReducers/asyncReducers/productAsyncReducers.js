0c8f6be134895c4e9084eeabd7df1ec53c5edd2c
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-19 21:48:02 +0100

    create the reducer and action for the product dispatch

--- client/src/reduxReducers/asyncReducers/productAsyncReducers.js
+++ client/src/reduxReducers/asyncReducers/productAsyncReducers.js
@@ -9,9 +9,9 @@ export const getProductLists = createAsyncThunk('product/getProductLists',
          //    host: 'localhost',
          //    port: 5000
          // }
-         const response = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
-         console.log(response)
-         return response.data.data
+         const { data } = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
+         // console.log(data)
+         return data.data
       } catch (err) {
          console.log(err.response)
          throw rejectWithValue(err.response)
@@ -19,9 +19,9 @@ export const getProductLists = createAsyncThunk('product/getProductLists',
    }
 )
 export const getProductById = createAsyncThunk('product/getProductById',
-   async (productId, { rejectWithValue, getState }) => {
+   async ({ _id }, { rejectWithValue, getState }) => {
       try {
-         const response = await axios.get(`/api/products/${productId}`)
+         const response = await axios.get(`/api/products/${_id}`)
          return response.data.data
       } catch (err) {
          console.log(err.response)
@@ -33,8 +33,8 @@ export const getProductById = createAsyncThunk('product/getProductById',
 export const getTopRatedProduct = createAsyncThunk('product/getTopRatedProduct',
    async (arg, { rejectWithValue }) => {
       try {
-         const response = await axios.get(`/api/products/top`)
-         return response.data.data
+         const { data } = await axios.get(`/api/products/top`)
+         return data.data
       } catch (err) {
          throw new rejectWithValue(err.response)
       }
@@ -42,18 +42,28 @@ export const getTopRatedProduct = createAsyncThunk('product/getTopRatedProduct',
 )
 
 export const createNewProduct = createAsyncThunk('product/createNewProduct',
-   async (newProduct, { rejectWithValue, getState }) => {
+   async ({ name, brand, category, description, price, image }, { rejectWithValue, getState }) => {
       try {
          const formData = new FormData()
-         formData.append()
+         formData.append('name', name)
+         formData.append('brand', brand)
+         formData.append('image', image)
+         formData.append('description', description)
+         formData.append('price', price)
+         formData.append('category', category)
+         const {
+            userLogin: {
+               userInfo
+            }
+         } = getState().User
          const config = {
             headers: {
                'Content-Type': 'multipart/form-data',
-               Authorization: `Bearer `
+               Authorization: `Bearer ${userInfo.token}`
             }
          }
-         const response = await axios.post(`/api/products`, formData, config)
-         return response.data.data
+         const { data } = await axios.post(`/api/products/`, formData, config)
+         return data.data
       } catch (err) {
          throw new rejectWithValue(err.response)
       }
@@ -61,9 +71,20 @@ export const createNewProduct = createAsyncThunk('product/createNewProduct',
 )
 
 export const deleteProduct = createAsyncThunk('product/deleteProduct',
-   async (productId, { rejectWithValue, getState }) => {
+   async ({ _id }, { rejectWithValue, getState }) => {
       try {
-
+         const {
+            userLogin: {
+               userInfo
+            }
+         } = getState().User
+         const config = {
+            headers: {
+               Authorization: `Bearer ${userInfo.token}`
+            }
+         }
+         const { data } = await axios.delete(`/api/products/${_id}`, config)
+         return data.data
       } catch (err) {
          throw new rejectWithValue(err.response)
       }
@@ -73,7 +94,29 @@ export const deleteProduct = createAsyncThunk('product/deleteProduct',
 export const updateProduct = createAsyncThunk('product/updateProduct',
    async (editedProduct, { rejectWithValue, getState }) => {
       try {
+         const { _id, name, image, category, description, price, countInStock } = editedProduct
+         const updatedProduct = {
+            name: name,
+            description: description,
+            category: category,
+            price: price,
+            image: image,
+            countInStock: countInStock
 
+         }
+         const {
+            userLogin: {
+               userInfo
+            }
+         } = getState().User
+         const config = {
+            headers: {
+               'Content-Type': 'multipart/form-data',
+               Authorization: `Bearer ${userInfo.token}`
+            }
+         }
+         const { data } = await axios.put(`/api/products/${_id}`, updatedProduct, config)
+         return data.data
       } catch (err) {
          throw new rejectWithValue(err.response)
       }
