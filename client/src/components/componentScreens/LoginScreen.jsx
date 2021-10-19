05ffc3d067349ee4457263e806f25a1b2736cd9c
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-19 21:54:32 +0100

    finish up with the product registration logic, login logic

--- client/src/components/componentScreens/LoginScreen.jsx
+++ client/src/components/componentScreens/LoginScreen.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { FloatingLabel, Form, Button, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import AppLoader from '../componentParts/AppLoader'
+import FormHousing from '../componentParts/FormHousing'
+import Message from '../componentParts/Message'
+import { useState } from 'react'
+import { loginUser } from '../../reduxReducers/asyncReducers/userAsyncReducers'
 
-const LoginScreen = () => {
+const LoginScreen = ({ location, history }) => {
+
+   const dispatch = useDispatch()
+   const {
+      error, loading, userLogin: {
+         userInfo
+      }
+   } = useSelector(state => state.User)
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const [msg, setMsg] = useState('')
+   const redirect = location.search ? location.search.split('=')[1] : '/'
+   useEffect(() => {
+      setMsg(error.msg)
+      if (userInfo) {
+         history.push(redirect)
+      }
+   }, [history, error, userInfo, redirect])
+   const loginData = {
+      email: email,
+      password: password
+   }
+   const onSubmitHandler = (e) => {
+      e.preventDefault()
+      dispatch(loginUser(loginData))
+   }
    return (
-      <div>
-      <h2>testing microphone </h2>
-         
-      </div>
+      <FormHousing>
+         <h1>Sign In</h1>
+         {msg ? <Message variant='danger'>{`${msg}`}</Message> : ''}
+         {loading && <AppLoader />}
+
+         <Form onSubmit={onSubmitHandler}>
+            <FloatingLabel className='mb-3' label='Email Address'>
+               <Form.Control
+                  type='email'
+                  name='email'
+                  placeholder='enter your mail here'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+               />
+            </FloatingLabel>
+            <FloatingLabel className='mb-3' label='Password'>
+               <Form.Control
+                  type='password'
+                  name='password'
+                  placeholder='enter your password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+               />
+            </FloatingLabel>
+            <Button
+               variant='outline-dark'
+               type='submit'
+               style={{ marginTop: '1.2rem' }}
+            >
+               Login
+            </Button>
+
+         </Form>
+         <Row>
+            <Col>
+               New Customer?{' '}
+               <Link to={redirect ? `/register?redirect=${redirect}` : '/register'} >
+                  Register
+               </Link>
+            </Col>
+         </Row>
+
+      </FormHousing>
+
    )
 }
 
--- client/src/components/componentScreens/RegistrationScreen.jsx
+++ client/src/components/componentScreens/RegistrationScreen.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Form, Row, Col, Button, FloatingLabel } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { registerUser } from '../../reduxReducers/asyncReducers/userAsyncReducers'
+import AppLoader from '../componentParts/AppLoader'
+import FormHousing from '../componentParts/FormHousing'
+import Message from '../componentParts/Message'
 
-const RegistrationScreen = () => {
+const RegistrationScreen = ({ location, history }) => {
+   const dispatch = useDispatch()
+   const [email, setEmail] = useState('')
+   const [name, setName] = useState('')
+   const [password, setPassword] = useState('')
+   const [msg, setMsg] = useState('')
+   const {
+      error, loading, userLogin: {
+         userInfo
+      }
+   } = useSelector(state => state.User)
+
+   const redirect = location.search ? location.search.split('=')[1] : '/'
+
+   useEffect(() => {
+      setMsg(error.msg)
+      if (userInfo) {
+         history.push(redirect)
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [error, history, userInfo])
+
+   const userData = {
+      name: name,
+      password: password,
+      email: email
+   }
+   const onSubmitHandler = (e) => {
+      e.preventDefault()
+      dispatch(registerUser(userData))
+   }
    return (
-      <div>
-      <h2>testing microphone </h2>
-         
-      </div>
+      <FormHousing>
+         <Form onSubmit={onSubmitHandler}>
+            <h1>User Registration</h1>
+            {msg ? <Message variant='danger'>{`${msg}`}</Message> : ''}
+            {loading && <AppLoader />}
+            <FloatingLabel className='mb-3' label='Full Name'>
+               <Form.Control
+                  type='text'
+                  name='name'
+                  placeholder='Enter your Full Name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+               />
+            </FloatingLabel>
+            <FloatingLabel className='mb-3' label='Email Address'>
+               <Form.Control
+                  type='text'
+                  name='name'
+                  placeholder='Enter your email address'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+               />
+            </FloatingLabel>
+            <FloatingLabel className='mb-3' label='Email Address'>
+               <Form.Control
+                  type='text'
+                  name='name'
+                  placeholder='Enter your password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+               />
+            </FloatingLabel>
+
+
+            <Button
+               type='submit'
+               variant='outline-dark'
+               style={{ marginTop: '1.2rem' }}
+            >
+               Register
+            </Button>
+         </Form>
+         <Row>
+            <Col>
+               Already Registered?{' '}
+               <Link to={redirect ? `/login?redirect=${redirect}` : '/login'} >
+                  login page
+               </Link>
+            </Col>
+         </Row>
+      </FormHousing>
    )
 }
 
