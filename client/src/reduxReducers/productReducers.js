3da6e1961ca4b5c374f6177263a5b75fc4211e7b
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-19 21:52:04 +0100

    modified the product and user reducers

--- client/src/reduxReducers/productReducers.js
+++ client/src/reduxReducers/productReducers.js
@@ -1,232 +1,62 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit"
-
+import { createSlice,/**nanoid*/ } from "@reduxjs/toolkit"
+import {
+   createNewProduct,
+   deleteProduct,
+   getProductById,
+   getProductLists,
+   getTopRatedProduct,
+   updateProduct
+} from "./asyncReducers/productAsyncReducers"
 
 
 export const ProductReducers = createSlice({
    name: 'product',
    initialState: {
-      products: [
-         {
-            _id: nanoid(),
-            name: 'Alexa',
-            image: '/images/alexa.jpg',
-            price: 40000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 20,
-            rating: 4,
-            reviews: [
-               { rating: 5, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-            ],
-            stockInCount: 0
-         },
-         {
-            _id: nanoid(),
-            name: 'Airpod',
-            image: '/images/airpods.jpg',
-            price: 40000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 1000,
-            rating: 3.7,
-            reviews: [
-               { rating: 5, comment: 'helping us',},
-               { rating: 5, comment: 'helping us',},
-               { rating: 5, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'Camera',
-            image: '/images/camera.jpg',
-            price: 20000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 10,
-            rating: 3.7,
-            reviews: [
-               { rating: 5, comment: 'helping us',},
-               { rating: 5, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'Mouse',
-            image: '/images/mouse.jpg',
-            price: 40000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 5,
-            rating: 0.5,
-            reviews: [
-               { rating: 0, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/phone.jpg',
-            price: 8000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 7,
-            rating: 4.7,
-            reviews: [
-               { rating: 3, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-               { rating: 4, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/playstation.jpg',
-            price: 5000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 3,
-            rating: 2.6,
-            reviews: [
-               { rating: 2, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/playstation.jpg',
-            price: 5000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 3,
-            rating: 2.6,
-            reviews: [
-               { rating: 2, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/playstation.jpg',
-            price: 5000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 3,
-            rating: 2.6,
-            reviews: [
-               { rating: 2, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/playstation.jpg',
-            price: 5000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 3,
-            rating: 2.6,
-            reviews: [
-               { rating: 2, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/playstation.jpg',
-            price: 5000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 3,
-            rating: 2.6,
-            reviews: [
-               { rating: 2, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-         {
-            _id: nanoid(),
-            name: 'iPhone',
-            image: '/images/playstation.jpg',
-            price: 5000,
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aliquam!',
-            numReviews: 3,
-            rating: 2.6,
-            reviews: [
-               { rating: 2, comment: 'helping us',},
-               { rating: 2, comment: 'helping us',},
-               { rating: 3, comment: 'helping us',},
-            ],
-            stockInCount: 12
-         },
-      ],
-      productSearch: [
-         {
-            _id: null,
-            name: '',
-            image: '',
-            price: null,
-            description: '',
-            numReviews: null,
-            rating: null,
-            reviews: [
-            ],
-            stockInCount: null
-
-         },
-
-      ],
+      products: [],
+      topProducts: [],
       product: {
-         _id: null,
-         name: '',
-         image: '',
-         price: null,
-         description: '',
-         numReviews: null,
-         rating: null,
-         reviews: [
-         ],
-         stockInCount: null
-
+         reviews: []
       },
       loading: false,
       status: false,
       error: null,
-      page: 2,
-      pages: 1
+      page: null,
+      pages: null
+      // productSearch: [
+      //    {
+      //       _id: null,
+      //       name: '',
+      //       image: '',
+      //       price: null,
+      //       description: '',
+      //       numReviews: null,
+      //       rating: null,
+      //       reviews: [
+      //       ],
+      //       stockInCount: null
+
+      //    },
+
+      // ],
+      // error: {
+      //    msg: undefined,
+      //    status: null,
+      //    id: null
+      // },
    },
    reducers: {
       getProduct: {
          reducer: (state, action) => {
             console.log(action.payload)
-            const { page, keyword} = action.payload
+            const { page, keyword } = action.payload
             // const regex = new RegExp(escapeRegex(keyword), 'gi')
             let refee = keyword ? {
                name: {
-                 $regex: escapeRegex(keyword),
-                 $options: 'i',
+                  $regex: escapeRegex(keyword),
+                  $options: 'i',
                },
-             } : {}
-            const productFound = state.products.slice((page-1)*10, page*10)
+            } : {}
+            const productFound = state.products.slice((page - 1) * 10, page * 10)
             console.log(refee)
             // const productFound = state.products.find(product => product.name === refee)
             const pages = Math.ceil(state.products.length / 10)
@@ -238,10 +68,10 @@ export const ProductReducers = createSlice({
                page: page
             }
          },
-         prepare : (keyword = '', pageNumber = '') => {
+         prepare: (keyword = '', pageNumber = '') => {
             const page = pageNumber
             return {
-               payload: { 
+               payload: {
                   page: page,
                   keyword: keyword
                }
@@ -249,12 +79,12 @@ export const ProductReducers = createSlice({
 
          }
       },
-      itemReviewCreation :  {
-         reducer: (state , action) => {
+      itemReviewCreation: {
+         reducer: (state, action) => {
             state.loading = false
-            const {_id, data} = action.payload
+            const { _id, data } = action.payload
             let selectedItem = state.products.find(product => product._id === _id)
-            if(selectedItem){
+            if (selectedItem) {
                selectedItem.reviews.push(data)
             }
             // return {
@@ -264,10 +94,10 @@ export const ProductReducers = createSlice({
             //    }
             // }
          },
-         prepare : (_id, {comment, rating}) => {
+         prepare: (_id, { comment, rating }) => {
             return {
                payload: {
-                  _id : _id,
+                  _id: _id,
                   data: {
                      comment: comment,
                      rating: rating
@@ -277,7 +107,7 @@ export const ProductReducers = createSlice({
 
          }
       },
-      getProductDetailsById : (state, action) => {
+      getProductDetailsById: (state, action) => {
          console.log(action.payload)
          return {
             ...state,
@@ -285,14 +115,179 @@ export const ProductReducers = createSlice({
          }
       }
    },
-   extraReducers:{
-
+   extraReducers: {
+      [getProductLists.pending]: (state, action) => {
+         state.loading = true
+      },
+      [getProductLists.fulfilled]: (state, action) => {
+         // console.log(action.payload)
+         return {
+            ...state,
+            loading: false,
+            products: action.payload.products,
+            pages: action.payload.pages,
+            page: action.payload.page
+         }
+      },
+      [getProductLists.rejected]: (state, action) => {
+         // console.log(action.payload)
+         return {
+            ...state,
+            loading: false,
+            status: false,
+            // error: {
+            //    msg: action.payload.data.message,
+            //    status: action.payload.status,
+            //    id: action.payload.statusText
+            // }
+            error: action.payload.data.message
+         }
+      },
+      [getProductById.pending]: (state, action) => {
+         return {
+            ...state,
+            loading: true
+         }
+      },
+      [getProductById.fulfilled]: (state, action) => {
+         // console.log(action.payload)
+         return {
+            ...state,
+            loading: false,
+            product: action.payload
+         }
+      },
+      [getProductById.rejected]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            status: false,
+            // error: {
+            //    msg: action.payload.data.message,
+            //    status: action.payload.status,
+            //    id: action.payload.statusText
+            // }
+            error: action.payload.data.message
+         }
+      },
+      [getTopRatedProduct.pending]: (state, action) => {
+         return {
+            ...state,
+            loading: false
+         }
+      },
+      [getTopRatedProduct.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            topProducts: action.payload
+         }
+      },
+      [getTopRatedProduct.rejected]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            status: false,
+            // error: {
+            //    msg: action.payload.data.message,
+            //    status: action.payload.status,
+            //    id: action.payload.statusText
+            // }
+            error: action.payload.data.message
+         }
+      },
+      [createNewProduct.pending]: (state, action) => {
+         return {
+            ...state,
+            loading: true
+         }
+      },
+      [createNewProduct.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            products: [action.payload, ...state.products]
+         }
+      },
+      [createNewProduct.rejected]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            status: false,
+            // error: {
+            //    msg: action.payload.data.message,
+            //    status: action.payload.status,
+            //    id: action.payload.statusText
+            // }
+            error: action.payload.data.message
+         }
+      },
+      [deleteProduct.pending]: (state, action) => {
+         return {
+            ...state,
+            loading: true
+         }
+      },
+      [deleteProduct.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            products: state.products.filter(product => product._id !== action.payload)
+         }
+      },
+      [deleteProduct.rejected]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            status: false,
+            // error: {
+            //    msg: action.payload.data.message,
+            //    status: action.payload.status,
+            //    id: action.payload.statusText
+            // }
+            error: action.payload.data.message
+         }
+      },
+      [updateProduct.pending]: (state, action) => {
+         return {
+            ...state,
+            loading: true
+         }
+      },
+      [updateProduct.fulfilled]: (state, action) => {
+         const { _id, name, description, category, price, image, countInStock } = action.payload
+         const existingProduct = state.products.find(product => product._id === _id)
+         if (existingProduct) {
+            existingProduct.name = name
+            existingProduct.description = description
+            existingProduct.price = price
+            existingProduct.image = image
+            existingProduct.category = category
+            existingProduct.countInStock = countInStock
+         }
+      },
+      [updateProduct.rejected]: (state, action) => {
+         return {
+            ...state,
+            loading: false,
+            status: false,
+            // error: {
+            //    msg: action.payload.data.message,
+            //    status: action.payload.status,
+            //    id: action.payload.statusText
+            // }
+            error: action.payload.data.message
+         }
+      }
    }
 })
+
+
+
 function escapeRegex(text) {
    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
 
-export const { itemReviewCreation, getProduct,getProductDetailsById } = ProductReducers.actions
+export const { itemReviewCreation, getProduct, getProductDetailsById } = ProductReducers.actions
 
 export default ProductReducers.reducer
--- client/src/reduxReducers/userReducers.js
+++ client/src/reduxReducers/userReducers.js
@@ -1,21 +1,244 @@
 import { createSlice } from "@reduxjs/toolkit"
+import {
+   deleteUser,
+   getUserById,
+   getUserProfile,
+   getUsers,
+   loginUser,
+   registerUser,
+   updateUser,
+   updateUserProfile
+} from "./asyncReducers/userAsyncReducers"
 
-
+const userInfoFromStorage = localStorage.getItem('userInfo')
+   ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 
 export const UserReducers = createSlice({
    name: 'user',
-   initialState:{
-
+   initialState: {
+      users: [],
+      user: {},
+      userLogin: {
+         userInfo: userInfoFromStorage
+      },
+      isAuthenticated: null,
+      isLoading: false,
+      error: {
+         msg: null,
+         status: null,
+         id: null
+      }
    },
    reducers: {
-
-   }, 
+      logoutSuccess: (state, action) => {
+         localStorage.removeItem('userInfo')
+         return {
+            ...state,
+            users: null,
+            userLogin: {
+               userInfo: userInfoFromStorage
+            },
+            isAuthenticated: false,
+            isLoading: false,
+            error: null
+         }
+      },
+      clearErrors: (state, action) => {
+         return {
+            ...state,
+            error: {}
+         }
+      }
+   },
    extraReducers: {
+      [getUsers.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+      },
+      [getUsers.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            users: action.payload
+         }
+      },
+      [getUsers.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            error: action.payload.data.message
+         }
+      },
+      [loginUser.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+
+      },
+      [loginUser.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            ...action.payload,
+            isAuthenticated: true,
+            userLogin: {
+               userInfo: action.payload
+            }
+         }
+
+      },
+      [loginUser.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            isAuthenticated: false,
+            userLogin: {
+               userInfo: null
+            },
+            error: {
+               msg: action.payload.data.message,
+               status: action.payload.status,
+               id: action.payload.statusText
+            }
+            // error: action.payload.data.message
+         }
+      },
+      [registerUser.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+      },
+      [registerUser.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            ...action.payload,
+            users: [action.payload, ...state.users],
+            isAuthenticated: true,
+            userLogin: {
+               userInfo: action.payload
+            }
+         }
+      },
+      [registerUser.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            isAuthenticated: false,
+            userLogin: {
+               userInfo: null
+            },
+            error: action.payload.data.message
+         }
+      },
+      [deleteUser.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+      },
+      [deleteUser.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            users: state.users.filter(user => user._id !== action.payload)
+         }
+      },
+      [deleteUser.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            error: action.payload.data.message
+         }
+      },
+      [updateUser.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+      },
+      [updateUser.fulfilled]: (state, action) => {
+         const { _id, email, name, isAdmin, status } = action.payload
+         const existingUser = state.users.find(user => user._id === _id)
+         if (existingUser) {
+            existingUser.isAdmin = isAdmin
+            existingUser.email = email
+            existingUser.name = name
+            existingUser.status = status
+         }
+      },
+      [updateUser.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            error: action.payload.data.message
+         }
+      },
+      [updateUserProfile.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+         }
+      },
+      [updateUserProfile.fulfilled]: (state, action) => {
+         const { _id, email, name, isAdmin, status } = action.payload
+         const existingUser = state.users.find(user => user._id === _id)
+         if (existingUser) {
+            existingUser.isAdmin = isAdmin
+            existingUser.email = email
+            existingUser.name = name
+            existingUser.status = status
+         }
+      },
+      [getUserById.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+
+      },
+      [getUserById.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            user: action.payload
+         }
+      },
+      [getUserById.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            error: action.payload.data.message
+         }
+      },
+      [getUserProfile.pending]: (state, action) => {
+         return {
+            ...state,
+            isLoading: true
+         }
+      },
+      [getUserProfile.fulfilled]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            user: action.payload
+         }
+      },
+      [getUserProfile.rejected]: (state, action) => {
+         return {
+            ...state,
+            isLoading: false,
+            error: action.payload.data.message
+         }
+
+      }
 
    }
 })
-
+export const { logoutSuccess, clearErrors } = UserReducers.actions
 
 
 export default UserReducers.reducer
