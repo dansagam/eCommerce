7b26bd2f0d59f19c7d7e2f91a6be021e74a7a603
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-21 23:51:16 +0100

    put up the userlist UI

--- client/src/components/componentScreens/UserListScreen.jsx
+++ client/src/components/componentScreens/UserListScreen.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import { faCheck, faEdit, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState } from 'react'
+import { Button, Table } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { deleteUser, getUsers } from '../../reduxReducers/asyncReducers/userAsyncReducers'
+import AppLoader from '../componentParts/AppLoader'
+import Message from '../componentParts/Message'
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
+   const [msg, setMsg] = useState('')
+   const {
+      error, loading, users, userLogin: {
+         userInfo
+      }
+   } = useSelector(state => state.User)
+
+   const dispatch = useDispatch()
+
+   useEffect(() => {
+      setMsg(error.msg)
+      if (userInfo && userInfo.isAdmin) {
+         dispatch(getUsers())
+      } else {
+         history.push('/login')
+      }
+
+   }, [error, userInfo, dispatch, history])
+
+   const deleteUserHandler = (id) => {
+      if (window.confirm('Are You Sure')) {
+         dispatch(deleteUser(id))
+      }
+   }
    return (
-      <div>
-      <h2>testing microphone UserList</h2>
-         
-      </div>
+      <>
+         <h1>User List</h1>
+         {msg ? <Message variant='danger'>{`${msg}`}</Message> : ''}
+         {loading ? (<AppLoader />)
+            : (
+               <Table>
+                  <thead>
+                     <tr>
+                        <td>NAME</td>
+                        <td>EMAIL</td>
+                        <td>ADMIN</td>
+                        <td></td>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {users.map(user => (
+                        <tr key={user._id}>
+                           <td>{user.name}</td>
+                           <td>
+                              <a href={`mailto:${user.email}`}>{user.email}</a>
+                           </td>
+                           <td>
+                              {user.isAdmin
+                                 ? (<FontAwesomeIcon icon={faCheck} />)
+                                 : (<FontAwesomeIcon icon={faTimes} />)
+                              }
+                           </td>
+                           <td>
+                              <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                                 <Button variant='light' className='btn-sm'>
+                                    <FontAwesomeIcon icon={faEdit} />
+                                 </Button>
+                              </LinkContainer>
+                              <Button
+                                 variant='light'
+                                 className='btn-sm'
+                                 onClick={() => deleteUserHandler(user._id)}
+                              >
+                                 <FontAwesomeIcon icon={faTrash} />
+                              </Button>
+                           </td>
+                        </tr>))
+                     }
+                  </tbody>
+               </Table>
+
+            )}
+
+      </>
    )
 }
 
--- client/src/components/componentScreens/UserProfileScreen.jsx
+++ client/src/components/componentScreens/UserProfileScreen.jsx
@@ -1,11 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Form, FloatingLabel, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import AppLoader from '../componentParts/AppLoader'
+import Message from '../componentParts/Message'
+import {
+   getUserProfile,
+   updateUserProfile
+} from '../../reduxReducers/asyncReducers/userAsyncReducers'
 
-const UserProfileScreen = () => {
+const UserProfileScreen = ({ location, history }) => {
+   const { userInfo } = useSelector(state => state.User.userLogin)
+   const { error, loading, user } = useSelector(state => state.User)
+   const dispatch = useDispatch()
+   const [name, setName] = useState('')
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const [confirmPassword, setConfirmPassword] = useState('')
+   const [msg, setMsg] = useState('')
+   const [message, setMessage] = useState('')
+
+   useEffect(() => {
+      setMsg(error.msg)
+      if (!userInfo) {
+         history.push('/login')
+      } else {
+         if (!user || !user.name) {
+            dispatch(getUserProfile({ _id: userInfo._id }))
+         } else {
+            setName(user.name)
+            setEmail(user.email)
+         }
+      }
+   }, [error, history, userInfo, dispatch, user])
+   const userUpdatedData = {
+      _id: user._id,
+      name: name,
+      email: email,
+      password: password
+   }
+
+   const onSubmitHandler = (e) => {
+      e.preventDefault()
+      if (confirmPassword !== password) {
+         setMessage('password Entered does not match')
+      } else {
+         dispatch(updateUserProfile(userUpdatedData))
+         setPassword('')
+         setConfirmPassword('')
+      }
+   }
    return (
-      <div>
-      <h2>testing microphone </h2>
-         
-      </div>
+      <Row>
+         <Col md={3}>
+            <h1>User Profile</h1>
+            {message ? <Message variant='danger'>{`${message}`}</Message> : ''}
+            {msg ? <Message variant='danger'>{`${msg}`}</Message> : ''}
+            {loading ? <AppLoader />
+               : (<Form onSubmit={onSubmitHandler}>
+                  <FloatingLabel label='Full Name'>
+                     <Form.Control
+                        type='text'
+                        name='name'
+                        placeholder='Enter your Full Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                     />
+                  </FloatingLabel>
+                  <FloatingLabel label='Email Address'>
+                     <Form.Control
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email address'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                     />
+                  </FloatingLabel>
+                  <FloatingLabel label='Password'>
+                     <Form.Control
+                        type='password'
+                        name='password'
+                        placeholder='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                     />
+                  </FloatingLabel>
+                  <FloatingLabel label='Confirm Password'>
+                     <Form.Control
+                        type='password'
+                        name='confirmpassword'
+                        placeholder='Confirm password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                     />
+                  </FloatingLabel>
+                  <Button type='submit'>Update Profile</Button>
+               </Form>
+               )}
+
+         </Col>
+      </Row>
    )
 }
 
