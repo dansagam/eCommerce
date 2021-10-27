752ad562c9c981e9832affbe069247dc0239dd4b
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-27 14:38:21 +0100

    finish the logic of cart Reducers

--- client/src/components/componentScreens/UserCartScreen.jsx
+++ client/src/components/componentScreens/UserCartScreen.jsx
@@ -1,48 +1,203 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Card, Col, Form, Image, ListGroup, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Message from '../componentParts/Message'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { addNewCart, deleteCartItem, getCartByUserId } from '../../reduxReducers/asyncReducers/cartAsyncReducers'
+import { useState } from 'react'
+import { Table } from 'react-bootstrap'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const UserCartScreen = ({ match, location, history }) => {
-   const productItemId = match.params.id
+   // const productItemId = match.params.id
    const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
    const dispatch = useDispatch()
-   const cart = useSelector(state => state.Cart)
-   const { cartItems } = cart
+   const cartState = useSelector(state => state.Cart)
+   const {
+      userLogin: {
+         userInfo
+      }
+   } = useSelector(state => state.User)
+   const { cartItems, error, cart } = cartState
    const checkoutHandler = () => {
-      // history.push('/login?redirect=shipping')
-      history.push('/shipping')
+      history.push('/login?redirect=shipping')
+      // history.push('/shipping')
    }
+   // console.log(qty)
+   const [msg, setMsg] = useState('')
+   const continueShopping = () => {
+      history.push('/')
+   }
+   const removeFromCartHandler = (id) => {
+      dispatch(deleteCartItem(id))
+   }
+   useEffect(() => {
+      setMsg(error.cart_error_msg)
+      if (!userInfo) {
+         history.push('/login')
+      } else {
+         if (!cart || cart.user !== userInfo._id) {
+            dispatch(getCartByUserId({ _id: userInfo._id }))
+         }
+      }
+   }, [history, cart, error, userInfo, match, dispatch])
 
    return (
       <div>
-         <h2>testing microphone </h2>
          <Row >
-            <Col md={8}>
-               <h1>Shipping Cart</h1>
+            <Col md={11}>
+               {msg ? <Message variant='danger'>{`${msg}`}</Message> : ''}
+               <div style={{ float: 'left' }}>
+                  <h3 style={{ textAlign: 'left', fontSize: '1.2rem', }}>
+                     Cart ({cartItems.reduce((acc, item) => (acc += item.qty), 0)}) items
+                  </h3>
+                  <h5 style={{ textAlign: 'left', fontSize: '0.9rem', }}>your order is free within Lagos</h5>
+               </div>
                {cartItems.length === 0 ? (<Message>Your Cart is Empty <Link to='/'>Go Back</Link></Message>)
-                  : <ListGroup variant='flush'>
+                  :
+                  (<Table className='table-sm' style={{ textAlign: 'left' }}>
+                     <thead>
+                        <tr>
+                           <th>ITEM</th>
+                           <th>QUANTITY</th>
+                           <th>UNIT PRICE</th>
+                           <th>SUBTOTAL</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        {cartItems.map((item) => (
+                           <tr key={item._id}>
+                              <td>
+                                 <Row>
+                                    <Col md={3}>
+                                       <Image src={item.product.image} alt={item.productName} fluid />
+                                    </Col>
+                                    <Col md={9}>
+                                       <Link to={`/product/${item.product._id}`}>{item.product.name}</Link>
+                                    </Col>
+                                 </Row>
+                                 <Row>
+                                    <Col md={3}></Col>
+                                    <Col md={4}>
+                                       <FontAwesomeIcon icon={faHeart} />
+                                       <span>  </span>  ADD TO FAVOURITE
+                                    </Col>
+                                    <Col md={5}>
+                                       <Button
+                                          type='button'
+                                          variant='light-outline'
+                                          onClick={() => removeFromCartHandler({ _id: cart._id, itemId: item._id })}
+                                       >
+                                          <FontAwesomeIcon icon={faTrash} />
+                                          <span>  </span> REMOVE
+                                       </Button>
+                                    </Col>
+                                 </Row>
+                              </td>
+                              <td>
+                                 <Row>
+                                    <Col >
+                                       <Form.Select
+                                          value={item.qty}
+                                          onChange={(e) =>
+                                             dispatch(addNewCart({
+                                                product_id: item.product._id,
+                                                qty: Number(e.target.value),
+                                                price: item.product.price
+                                             }))
+                                          }
+                                       >
+                                          {[...Array(item.product.countInStock).keys()].map((x) => (
+                                             <option key={x + 1} value={x + 1}>
+                                                {x + 1}
+                                             </option>
+                                          ))}
+                                       </Form.Select>
+                                    </Col>
+                                 </Row>
+                                 <Row></Row>
+                              </td>
+                              <td>
+                                 ₦{item.product.price}
+                              </td>
+                              <td>
+                                 {(item.product.price * item.qty).toFixed(2)}
+                              </td>
+                           </tr>
+                        ))}
+                     </tbody>
+                     <tfoot>
+                        <tr>
+                           <td colSpan={2}></td>
+                           <td colSpan={2}>
+                              Total: <strong>
+                                 ₦ {cartItems.reduce((acc, item) =>
+                                    acc += (item.qty * item.product.price), 0).toFixed(2)
+                                 }
+                              </strong>
+                           </td>
+                        </tr>
+                        <tr>
+                           <td colSpan={1} style={{ textAlign: 'right' }}>
+                              <Button
+                                 className='btn-block'
+                                 onClick={continueShopping}
+                              >
+                                 CONTINUE SHOPPPING
+                              </Button>
+                           </td>
+                           <td colSpan={3}>
+                              <Button
+                                 className='btn-block'
+                                 disabled={cartItems.length === 0}
+                                 onClick={checkoutHandler}
+                              >
+                                 Proceed to Checkout
+                              </Button>
+                           </td>
+                        </tr>
+                     </tfoot>
+                  </Table>
+                  )
+               }
+            </Col>
+         </Row>
+
+      </div>
+   )
+}
+
+export default UserCartScreen
+
+
+
+const gie = {/** (<ListGroup variant='flush'>
                      {cartItems.map((item) => (
-                        <ListGroup.Item key={item.productId}>
-                           {console.log(item)}
+                        <ListGroup.Item key={item._id}>
                            <Row>
                               <Col md={2}>
-                                 <Image src={item.productImage} alt={item.productName} fluid />
+                                 <Image src={item.product.image} alt={item.productName} fluid />
                               </Col>
                               <Col md={3}>
-                                 <Link to={`/product/${item.productId}`}>sss{item.productName}</Link>
+                                 <Link to={`/product/${item.product._id}`}>{item.product.name}</Link>
                               </Col>
-                              <Col md={2}>₦{item.productPrice}</Col>
+                              <Col md={2}>₦{item.product.price}</Col>
                               <Col md={2}>
                                  <Form.Select
                                     value={item.qty}
+                                    onChange={(e) =>
+                                       dispatch(addNewCart({
+                                          product_id: item.product._id,
+                                          qty: Number(e.target.value),
+                                          price: item.product.price
+                                       }))
+                                    }
                                  >
-                                    {[...Array(item.productStockCount).keys()].map((x) => (
+                                    {[...Array(item.product.countInStock).keys()].map((x) => (
                                        <option key={x + 1} value={x + 1}>
                                           {x + 1}
                                        </option>
@@ -50,23 +205,24 @@ const UserCartScreen = ({ match, location, history }) => {
                                  </Form.Select>
                               </Col>
                               <Col md={2}>
-                                 <Button>
+                                 <Button
+                                    type='button'
+                                    variant='light'
+                                    onClick={() => removeFromCartHandler({ _id: cart._id, itemId: item._id })}
+                                 >
                                     <FontAwesomeIcon icon={faTrash} />
                                  </Button>
                               </Col>
                            </Row>
                         </ListGroup.Item>
                      ))}
-                     <h1>testing</h1>
-                  </ListGroup>
-               }
-            </Col>
-            <Col md={4}>
+                     </ListGroup>
+                  : (<Col md={4}>
                <Card>
                   <ListGroup variant='flush'>
                      <ListGroup.Item>
-                        <h2>Subtotal: ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
-                        ₦ {cartItems.reduce((acc, item) => acc + item.qty * item.productPrice, 0).toFixed(2)}
+                        <h2>Cart: ({cartItems.reduce((acc, item) => (acc += item.qty), 0)}) items</h2>
+                        ₦ {cartItems.reduce((acc, item) => acc += (item.qty * item.product.price), 0).toFixed(2)}
                      </ListGroup.Item>
                      <ListGroup.Item>
                         <Button
@@ -79,11 +235,4 @@ const UserCartScreen = ({ match, location, history }) => {
                      </ListGroup.Item>
                   </ListGroup>
                </Card>
-            </Col>
-         </Row>
-
-      </div>
-   )
-}
-
-export default UserCartScreen
+            </Col> */}
