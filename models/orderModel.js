189b1f59966ec9d08474010a02504ea564e8335f
dansagam <olukayodeogunnowo@yahoo.com>
2021-10-31 23:26:06 +0100

    modified the order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,30 +1,76 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User'
+   {
+      user: {
+         type: mongoose.Schema.Types.ObjectId,
+         required: true,
+         ref: 'User'
 
-    },
-    cart: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'Cart'
-    },
-    isDelivered: {
-      type: Boolean,
-      required: true,
-      default: false
-    },
-    deliveredAt: {
-      type: Date
-    }
-  },
-  {
-    timestamps: true,
-  }
+      },
+      cart: {
+         type: mongoose.Schema.Types.ObjectId,
+         required: true,
+         ref: 'Cart'
+      },
+      orderItems: [
+         {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'CartItem'
+         }
+      ],
+      paymentMethod: {
+         type: String,
+         enum: ['Paypal', 'FlutterWave', 'Stripe', 'On Delivery'],
+         required: true,
+         default: 'On Delivery',
+
+      },
+      shippingAddress: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'ShippingAddress'
+      },
+      isDelivered: {
+         type: Boolean,
+         required: true,
+         default: false
+      },
+      deliveredAt: {
+         type: Date
+      },
+      paymentResult: {
+         id: { type: String },
+         status: { type: String },
+         update_time: { type: String },
+         email_address: { type: String }
+      },
+      taxPrice: {
+         type: Number,
+         required: true,
+         default: 0.0
+      },
+      shippingPrice: {
+         type: Number,
+         required: true,
+         default: 0.0
+      },
+      totalPrice: {
+         type: Number,
+         required: true,
+         default: 0.0
+      },
+      isPaid: {
+         type: Boolean,
+         required: true,
+         default: false
+      },
+      paidAt: {
+         type: Date
+      },
+   },
+   {
+      timestamps: true,
+   }
 );
 const Order = mongoose.model('Order', orderSchema)
 export default Order;
